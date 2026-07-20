168bb0b2e9099c027f87a18f950c60772780a47f
Jose Rodriguez <1005.31432935.ucla@gmail,com>
2026-07-20 18:46:39 -0400

    nuevo correo

--- appMikrotik/appMikrotik/settings.py
+++ appMikrotik/appMikrotik/settings.py
@@ -151,8 +151,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'correoinstagram143@gmail.com'
-EMAIL_HOST_PASSWORD = 'gvnk jyzc detl vlmb'
+EMAIL_HOST_USER = 'mikrocore14@gmail.com'
+EMAIL_HOST_PASSWORD = 'xddj fdaf yphy jvxm'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # --- Celery ---
